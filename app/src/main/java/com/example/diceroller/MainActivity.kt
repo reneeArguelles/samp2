d62bff322b66efe650ead7634e473068f33f834c
reneeArguelles <mdarguelles@up.edu.ph>
2020-02-06 18:52:31 +0800

    samp2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,39 +3,39 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
-
-        val countButton: Button = findViewById(R.id.count_button)
-        countButton.setOnClickListener{ countUp()}
     }
 
     private fun rollDice(){
         val randomInt = (1..6).random()
-        //Toast.makeText(this, "button clicked",
-        //   Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        //val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
-    private fun countUp(){
-        val textInView: TextView = findViewById(R.id.result_text)
-
-        if(textInView.text != "Hello World!"){
-            var holder = textInView.text.toString().toInt()
-            if(holder<6) {
-                holder += 1
-                textInView.text = holder.toString()
-            }
-        }else textInView.text = "1"
-    }
 }
